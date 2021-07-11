f11e5750c867c7be724b1d6f7088d5a6e9b4e7a4
Pedro Trujillo <ptrujillo@velocityz.com>
2021-07-11 00:32:11 -0400

    Added new view model feature

--- WorkoutListApp/ViewModel/ExerciseViewModel.swift
+++ WorkoutListApp/ViewModel/ExerciseViewModel.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Pedro Enrique Trujillo Vargas on 7/9/21.
 //
+//"https://wger.de/api/v2/exerciseimage/\(id)/?format=json&language=2" // full image
+
 
 import Foundation
 import UIKit
@@ -33,7 +35,23 @@ class ExerciseViewModel {
         if imageModel.thumbnail.url == "" { return "" }
         return baseURLString + imageModel.thumbnail.url
     }
-    //https://wger.de/api/v2/exerciseimage/167/?format=json&language=2 // full image
+    
+    var fullImage: String {
+        guard let imageModel = imageModel else {
+            print("\n ⚠️ ExerciseViewModel.fullImage: not image model for exercise \(id) \(name)")
+            return ""
+        }
+        guard let original = imageModel.original else {
+            print("\n ⚠️ ExerciseViewModel.fullImage : not original for exercise \(id) \(name)")
+            return ""
+        }
+        if original == "" { return "" }
+        return baseURLString + original
+    }
+    
+    var categoryId: Int {
+        return model.category ?? -1
+    }
     
     public func getImageURL(completion: @escaping (URL) -> Void) {
         let thumbnail = self.thumbnail
@@ -45,6 +63,24 @@ class ExerciseViewModel {
                 return
             }
             completion(url)
+        }
+    }
+    
+    public func getCategoryName( completion: @escaping (String)-> Void) {
+        DataSource.retrieveCategory(by: categoryId) { categoryName in
+            completion(categoryName)
+        }
+    }
+    
+    public func  getOtherInfo( completion: @escaping (String)-> Void) {
+        var equipment = ""
+        
+        self.getCategoryName { categoryName in
+            DispatchQueue.main.async {
+                let text = "Category: " + categoryName + "\nEquipment: " + equipment
+                completion(text)
+            }
+            
         }
     }
     
